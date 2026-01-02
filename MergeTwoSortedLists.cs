a02dab378485ead86bfa8908b2199359e0d46a14
ojas2005 <tiwariojas4@gmail.com>
2026-01-02 20:13:45 +0530

    Solved Merge Two Sorted Lists Question

---
+++ MergeTwoSortedLists.cs
@@ -0,0 +1,30 @@
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0,ListNode next=null){this.val=val;this.next=next;}
+}
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode l1,ListNode l2)
+    {
+        ListNode dummy=new ListNode(0);
+        ListNode curr=dummy;
+        while(l1!=null&&l2!=null)
+        {
+            if(l1.val<=l2.val)
+            {
+                curr.next=l1;
+                l1=l1.next;
+            }
+            else
+            {
+                curr.next=l2;
+                l2=l2.next;
+            }
+            curr=curr.next;
+        }
+        curr.next=l1!=null?l1:l2;
+        return dummy.next;
+    }
+}
