f4b3fa0d1620f462a13da99ce356471ab6395b60
ojas2005 <tiwariojas4@gmail.com>
2026-01-23 16:42:09 +0530

    Solved Contains Duplicate II using sliding window approach

---
+++ ContainsDuplicateII.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums,int k)
+    {
+        Dictionary<int,int> map=new Dictionary<int,int>();
+        for(int i=0;i<nums.Length;i++)
+        {
+            if(map.ContainsKey(nums[i])&&i-map[nums[i]]<=k)return true;
+            map[nums[i]]=i;
+        }
+        return false;
+    }
+}
