2178e49713d4b29c178f092867f63134231b8ce5
ojas2005 <tiwariojas4@gmail.com>
2026-01-23 18:53:28 +0530

    Solved Missing Number using sum formula

---
+++ MissingNumber.cs
@@ -0,0 +1,10 @@
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n=nums.Length;
+        int sum=n*(n+1)/2;
+        for(int i=0;i<n;i++)sum-=nums[i];
+        return sum;
+    }
+}
