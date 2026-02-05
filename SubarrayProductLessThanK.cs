145b3063b2c15ba6bd8bcc7254d0cfb2f409525e
ojas2005 <tiwariojas4@gmail.com>
2026-02-05 08:28:14 +0530

    Solved Subarray Product Less Than K

---
+++ SubarrayProductLessThanK.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public int NumSubarrayProductLessThanK(int[] nums,int k)
+    {
+        if(k<=1)return 0;
+        int left=0;
+        long product=1;
+        int count=0;
+        for(int right=0;right<nums.Length;right++)
+        {
+            product*=nums[right];
+            while(product>=k)
+            {
+                product/=nums[left];
+                left++;
+            }
+            count+=right-left+1;
+        }
+        return count;
+    }
+}
