5a7a29b5545c207d843e0b6245742c414be10c82
ojas2005 <tiwariojas4@gmail.com>
2026-02-06 18:42:09 +0530

    Solved Longest Subarray of 1's After Deleting One Element

---
+++ LongestSubarrayOfOnesAfterDeletingOneElement.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public int LongestSubarray(int[] nums)
+    {
+        int left=0,zeros=0,maxLen=0;
+        for(int right=0;right<nums.Length;right++)
+        {
+            if(nums[right]==0)zeros++;
+            while(zeros>1)
+            {
+                if(nums[left]==0)zeros--;
+                left++;
+            }
+            maxLen=Math.Max(maxLen,right-left);
+        }
+        return maxLen;
+    }
+}
