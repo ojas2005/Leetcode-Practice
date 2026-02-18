bd10f32317b36c10f079234af30e35458e2053fe
ojas2005 <tiwariojas4@gmail.com>
2026-02-18 08:33:27 +0530

    Solved Jump Game II using greedy

---
+++ JumpGameII.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public int Jump(int[] nums)
+    {
+        int jumps=0,currEnd=0,farthest=0;
+        for(int i=0;i<nums.Length-1;i++)
+        {
+            farthest=Math.Max(farthest,i+nums[i]);
+            if(i==currEnd)
+            {
+                jumps++;
+                currEnd=farthest;
+            }
+        }
+        return jumps;
+    }
+}
