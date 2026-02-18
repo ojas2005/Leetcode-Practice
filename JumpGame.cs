3d7cce5f9ea52af0e07f67e0c6a624a8e62f81b4
ojas2005 <tiwariojas4@gmail.com>
2026-02-18 16:58:13 +0530

    Solved Jump Game

---
+++ JumpGame.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        int reach=0;
+        for(int i=0;i<nums.Length;i++)
+        {
+            if(i>reach)return false;
+            reach=Math.Max(reach,i+nums[i]);
+        }
+        return true;
+    }
+}
