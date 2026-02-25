bdc2560fece38ed8251c874b8007e7b494dd900b
ojas2005 <tiwariojas4@gmail.com>
2026-02-25 17:44:19 +0530

    Solved Subsets II

---
+++ SubsetsII.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public IList<IList<int>> SubsetsWithDup(int[] nums)
+    {
+        Array.Sort(nums);
+        List<IList<int>> res=new List<IList<int>>();
+        Backtrack(nums,0,new List<int>(),res);
+        return res;
+    }
+    private void Backtrack(int[] nums,int start,List<int> curr,List<IList<int>> res)
+    {
+        res.Add(new List<int>(curr));
+        for(int i=start;i<nums.Length;i++)
+        {
+            if(i>start&&nums[i]==nums[i-1])continue;
+            curr.Add(nums[i]);
+            Backtrack(nums,i+1,curr,res);
+            curr.RemoveAt(curr.Count-1);
+        }
+    }
+}
