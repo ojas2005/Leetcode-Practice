80a4041210c1a7045ec32d19550daa5219fdcf35
ojas2005 <tiwariojas4@gmail.com>
2026-06-27 09:45:11 +0530

    Solved Search Insert Position using binary search

---
+++ SearchInsertPosition.cs
@@ -0,0 +1,15 @@
+public class Solution
+{
+    public int SearchInsert(int[] nums,int target)
+    {
+        int lo=0,hi=nums.Length-1;
+        while(lo<=hi)
+        {
+            int mid=(lo+hi)/2;
+            if(nums[mid]==target)return mid;
+            if(nums[mid]<target)lo=mid+1;
+            else hi=mid-1;
+        }
+        return lo;
+    }
+}
